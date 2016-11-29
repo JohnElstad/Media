dae39306317201b7d1a4bfb38a744f308abb26de
johnelstad <johnelstad1@gmail.com>
2016-11-29 06:59:11 -0800

    initial

---
+++ Media/.#genericType.h
@@ -0,0 +1,1 @@
+Flateye@ELYSIUS.62
---
+++ Media/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+// John Elstad, 11/28/16, This is a thing that keeps track of stuff. 
+int main(){
+  
+  char input[10];
+  cout<<"Input a Command. Valid Commands are ADD, SEARCH, DELETE and EXIT"<<endl;
+  cin>>input;
+  if(strcmp(input,"ADD")==0){
+    cout<<"You chose add"<<endl;
+  }
+  else if(1){
+
+  }
+  else if(1){
+
+  }
+  else if(1){
+
+  }
+  else{
+    cout<<"Thats not a valid Command"<<endl;
+  }
+
+}
